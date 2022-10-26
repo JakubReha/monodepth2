cad1030a976667591316c68a1d4f82985ffd4749
JakubReha <kubareha107@gmail.com>
2022-10-26 22:02:32 +0800

    evalute pose on the iPhone dataset

---
+++ datasets/iPhone.py
@@ -0,0 +1,53 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+import skimage.transform
+import numpy as np
+import PIL.Image as pil
+
+from kitti_utils import generate_depth_map
+from .mono_dataset import MonoDataset
+
+
+class IPHONEDataset(MonoDataset):
+    """Superclass for different types of KITTI dataset loaders
+    """
+    def __init__(self, *args, **kwargs):
+        super(IPHONEDataset, self).__init__(*args, **kwargs)
+
+        # NOTE: Make sure your intrinsics matrix is *normalized* by the original image size.
+        # To normalize you need to scale the first row by 1 / image_width and the second row
+        # by 1 / image_height. Monodepth2 assumes a principal point to be exactly centered.
+        # If your principal point is far from the center you might need to disable the horizontal
+        # flip augmentation.
+        """self.K = np.array([[0.58, 0, 0.5, 0],
+                           [0, 1.92, 0.5, 0],
+                           [0, 0, 1, 0],
+                           [0, 0, 0, 1]], dtype=np.float32)"""
+
+        #self.full_res_shape = (1242, 375)
+        #self.side_map = {"2": 2, "3": 3, "l": 2, "r": 3}
+
+
+    def get_color(self, folder, frame_index, side, do_flip):
+        color = self.loader(self.get_image_path(folder, frame_index, side))
+
+        if do_flip:
+            color = color.transpose(pil.FLIP_LEFT_RIGHT)
+
+        return color
+
+
+class IPHONEOdomDataset(IPHONEDataset):
+    """IPHONE dataset for odometry training and testing
+    """
+    def __init__(self, *args, **kwargs):
+        super(IPHONEOdomDataset, self).__init__(*args, **kwargs)
+
+    def get_image_path(self, folder, frame_index, side):
+        f_str = "{:04d}{}".format(frame_index, ".jpg")
+        image_path = os.path.join(
+            self.data_path,
+            "img",
+            f_str)
+        return image_path
--- evaluate_pose.py
+++ evaluate_pose.py
@@ -15,7 +15,7 @@ from torch.utils.data import DataLoader
 from layers import transformation_from_parameters
 from utils import readlines
 from options import MonodepthOptions
-from datasets import KITTIOdomDataset
+from datasets import IPHONEOdomDataset
 import networks
 
 
@@ -55,13 +55,9 @@ def evaluate(opt):
     assert opt.eval_split == "odom_9" or opt.eval_split == "odom_10", \
         "eval_split should be either odom_9 or odom_10"
 
-    sequence_id = int(opt.eval_split.split("_")[1])
+    filenames = readlines("/content/drive/MyDrive/Project/Dataset/Videos/iPhone8/attrium_2_out/files.txt")
 
-    filenames = readlines(
-        os.path.join(os.path.dirname(__file__), "splits", "odom",
-                     "test_files_{:02d}.txt".format(sequence_id)))
-
-    dataset = KITTIOdomDataset(opt.data_path, filenames, opt.height, opt.width,
+    dataset = IPHONEOdomDataset("/content/drive/MyDrive/Project/Dataset/Videos/iPhone8/attrium_2_out/", filenames, opt.height, opt.width,
                                [0, 1], 4, is_train=False)
     dataloader = DataLoader(dataset, opt.batch_size, shuffle=False,
                             num_workers=opt.num_workers, pin_memory=True, drop_last=False)
@@ -100,29 +96,6 @@ def evaluate(opt):
                 transformation_from_parameters(axisangle[:, 0], translation[:, 0]).cpu().numpy())
 
     pred_poses = np.concatenate(pred_poses)
-
-    gt_poses_path = os.path.join(opt.data_path, "poses", "{:02d}.txt".format(sequence_id))
-    gt_global_poses = np.loadtxt(gt_poses_path).reshape(-1, 3, 4)
-    gt_global_poses = np.concatenate(
-        (gt_global_poses, np.zeros((gt_global_poses.shape[0], 1, 4))), 1)
-    gt_global_poses[:, 3, 3] = 1
-    gt_xyzs = gt_global_poses[:, :3, 3]
-
-    gt_local_poses = []
-    for i in range(1, len(gt_global_poses)):
-        gt_local_poses.append(
-            np.linalg.inv(np.dot(np.linalg.inv(gt_global_poses[i - 1]), gt_global_poses[i])))
-
-    ates = []
-    num_frames = gt_xyzs.shape[0]
-    track_length = 5
-    for i in range(0, num_frames - 1):
-        local_xyzs = np.array(dump_xyz(pred_poses[i:i + track_length - 1]))
-        gt_local_xyzs = np.array(dump_xyz(gt_local_poses[i:i + track_length - 1]))
-
-        ates.append(compute_ate(gt_local_xyzs, local_xyzs))
-
-    print("\n   Trajectory error: {:0.3f}, std: {:0.3f}\n".format(np.mean(ates), np.std(ates)))
 
     save_path = os.path.join(opt.load_weights_folder, "poses.npy")
     np.save(save_path, pred_poses)
